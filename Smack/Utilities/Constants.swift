563e3d4d5c943cceaf51a9db6d7a6fbf2e1647a6
Christyan Duarte <christyan.duarte@globant.com>
2019-08-07 13:41:37 -0500

    Lesson 4.

---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Christyan Huber Duarte Ibanez on 8/7/19.
+//  Copyright © 2019 Christyan Huber Duarte Ibanez. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
